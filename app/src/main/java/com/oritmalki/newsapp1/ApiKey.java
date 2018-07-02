c24d4d86a0daa061682a3b85effcdc124c25840a
oritgurel <oritgurel@gmail.com>
2018-07-02 22:05:37 +0300

    Update ApiKey.java

--- app/src/main/java/com/oritmalki/newsapp1/ApiKey.java
+++ app/src/main/java/com/oritmalki/newsapp1/ApiKey.java
@@ -4,6 +4,7 @@ public class ApiKey {
     private static final String apiKey = BuildConfig.MY_GUARDIAN_API_KEY;
 
     public static String getApiKey() {
+        //return your own api key or 'test' api key for the code to compile.
         return apiKey;
     }
 }
